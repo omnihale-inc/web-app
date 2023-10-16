fd9781994f543a80046ff08167c1e8061fb9f0a7
merhmood <mahmudsuberu9@gmail.com>
2023-10-16 20:50:24 +0100

    fix: add cors to api and reorganize email template

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -3,10 +3,20 @@ type EmailTemplateProps = {
 };
 
 export default function EmailTemplate(props: EmailTemplateProps) {
+  const contents: Object = JSON.parse(props.emailContents);
   return (
     <div>
       <h1>Feed Back</h1>
-      <p>{props.emailContents}</p>
+      {Object.entries(contents).map((content) => {
+        return (
+          <div key={content[0]}>
+            {/* name */}
+            <span>{content[0]}:</span>
+            {/* value */}
+            <span>{content[1]}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
