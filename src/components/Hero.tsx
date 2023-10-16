e531cae5c3bb5f418c78875e87695e99a2a0a07c
gracebir <grace.devolop@gmail.com>
2023-10-16 09:49:39 +0200

    animate hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-
+import { motion } from 'framer-motion'
 import Button from "./Elements/Button";
+import { staggerContainer, textVariant } from '@/utilities/motion';
 
 type HeroProps = {
   language: {
@@ -19,26 +20,35 @@ type HeroProps = {
 
 export default function Hero(props: HeroProps) {
   return (
-    <div className='max-w-[1250px] w-[90%] m-auto'>
-      <div>
-        <h1 className='text-center text-xl lg:text-5xl max-w-[900px] w-[90%] m-auto text-[#247e5b] font-bold'>
+    <motion.div
+    variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{once: false, amount: 0.25}}
+     className='max-w-[1250px] w-[90%] m-auto'>
+      <motion.div
+        
+      >
+        <motion.h1
+         variants={textVariant(1)}
+         className='text-center text-xl lg:text-5xl max-w-[900px] w-[90%] m-auto text-[#247e5b] font-bold'>
           {props.language.pageContent.hero.title}
-        </h1>
-        <p className='text-center text-sm lg:text-xl max-w-[900px] w-[90%] m-auto mt-5'>
+        </motion.h1>
+        <motion.p  variants={textVariant(1.1)} className='text-center text-sm lg:text-xl max-w-[900px] w-[90%] m-auto mt-5'>
           {props.language.pageContent.hero.description}
-        </p>
+        </motion.p>
         <Button text={props.language.pageContent.hero.button} handleClick={() => (location.href = 'mailto:contact@omnihale.com')}/>
-      </div>
-      <div className='w-[fit-content] m-auto'>
+      </motion.div>
+      <div className='w-[fit-content] m-auto duration-300'>
         <img
           src='/images/First aid kit-rafiki.png'
           alt='first aid'
-          className='w-[125px]  2xl:inline hidden'
+          className='w-[125px]  2xl:inline hidden duration-300 hover:translate-x-3'
         />
         <img
           src='/images/Hospital patient-bro.png'
           alt='first aid'
-          className='w-[250px]  xl:inline hidden'
+          className='w-[250px]  xl:inline hidden duration-300 hover:-translate-x-3'
         />
         <img
           src='/images/Online Doctor-bro.png'
@@ -48,14 +58,14 @@ export default function Hero(props: HeroProps) {
         <img
           src='/images/Cardiologist-bro.png'
           alt='first aid'
-          className='w-[250px]  xl:inline hidden'
+          className='w-[250px]  xl:inline hidden duration-300 hover:translate-x-3'
         />
         <img
           src='/images/Online Doctor-rafiki.png'
           alt='first aid'
-          className='w-[125px] 2xl:inline hidden'
+          className='w-[125px] 2xl:inline hidden duration-300 hover:-translate-x-3'
         />
       </div>
-    </div>
+    </motion.div>
   );
 }
