fb7e3219269949ea27c3673c5f92e99ba8de83e9
merhmood <mahmudsuberu9@gmail.com>
2023-11-14 23:28:42 +0100

    feat: added trying resend fix with ben idea

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -1,12 +1,19 @@
 type EmailTemplateProps = {
+  email: string;
   emailContents: string;
 };
 
 export default function EmailTemplate(props: EmailTemplateProps) {
+  const email: [string, string][] = Object.entries(
+    JSON.parse(props.emailContents)
+  );
   const contents: Object = JSON.parse(props.emailContents);
   return (
     <div>
       <h1>Feed Back</h1>
+      <div>
+        {email[0][0]}:{email[0][1]}
+      </div>
       {Object.entries(contents).map((content) => {
         return (
           <div key={content[0]}>
